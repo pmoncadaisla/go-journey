0b35f0026f2cd7123753655eb08c2da533fe7c65
Pablo Moncada <pablo.moncada@bq.com>
2018-03-26 19:58:34 +0200

    Added storage driver comments

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import "github.com/pmoncadaisla/go-journey/pkg/domain"
 
+// Interface must be implemented by all storage drivers
 type Interface interface {
 	Store(*domain.Journey)
 }
